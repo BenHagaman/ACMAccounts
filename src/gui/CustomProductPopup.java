dedb70923ab10d087dc26233a709810354c62ca7
Ben Hagaman <haga0062@umn.edu>
2012-12-30 18:03:50 -0600

    trying to add project files

--- src/gui/CustomProductPopup.java
+++ src/gui/CustomProductPopup.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -73,7 +72,6 @@ class SampleDialog {
         this.dialog = new JDialog(this.parentFrame, "Sample", true);
         this.dialog.getContentPane().add(createPane());
         this.dialog.getRootPane().setDefaultButton(this.okButton);
-        setEscapeKeyMap();
         this.dialog.pack();
         this.dialog.setLocationRelativeTo(this.parentFrame);
     }
@@ -97,22 +95,6 @@ class SampleDialog {
         return mainPanel;
     }
 
-    private void setEscapeKeyMap(){
-        String CANCEL_ACTION_KEY = "CANCEL_ACTION_KEY";
-        int noModifiers = 0;
-        Object KeyEvent = null;
-
-        InputMap inputMap = this.dialog.getRootPane().getInputMap(
-                JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-        AbstractAction cancelAction = new AbstractAction(){
-            public void actionPerformed(ActionEvent e){
-                close();
-            }
-        };
-        this.dialog.getRootPane().getActionMap().put(
-                CANCEL_ACTION_KEY, cancelAction);
-    }
-
     public void show() {
         if (this.dialog == null) {
             init();
@@ -134,71 +116,3 @@ class SampleDialog {
         }
     }
 }
-
-//
-//public class CustomDialog extends JFrame {
-//    CustomDialog(String title){
-//        super(title);
-//        addWindowListener(
-//                new WindowAdapter() {
-//                    public void windowClosing(WindowEvent e) {
-//                        dispose();
-//                        System.exit(0);
-//                    }
-//                }
-//        );
-//
-//        String[] labels = {"Name","Value"};
-//        String[] defaults = {"",""};
-//        MultilineDialogPanel panel = new
-//                MultilineDialogPanel(labels,defaults,20);
-//        int selection = JOptionPane.showConfirmDialog(this,panel,"Multiline" +
-//                "Dialog",JOptionPane.OK_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE);
-//        if (selection==JOptionPane.OK_OPTION){
-//            System.out.println(panel.getResults()[0]);
-//            System.out.println(panel.getResults()[1]);
-//        }
-//    }
-//    public static void main(String[] args){
-//        CustomDialog dlg = new CustomDialog("Multiline dialog");
-//        dlg.show();
-//    }
-//}
-//
-//
-//class MultilineDialogPanel extends JPanel{
-//    GridBagConstraints gbc = new GridBagLayout();
-//    GridBagLayout gbl = new GridBagConstraints();
-//    ArrayList options = new ArrayList();
-//    MultilineDialogPanel(String[] labels,String[] defaults,int
-//            fieldLength){
-//        super();
-//        if (fieldLength == -1){fieldLength = 20;};
-//        this.setLayout(gbl);
-//        for (int i =0;i<labels.length;i++){
-//            addComponent(new
-//                    JLabel(labels[i]),0,i*2,GridBagConstraints.WEST);
-//            JTextField text = new JTextField(defaults[i],fieldLength);
-//            addComponent(text,0,i*2+1,GridBagConstraints.WEST);
-//            options.add(text);
-//        }
-//    }
-//    public String[] getResults(){
-//        String[] results = new String[options.size()];
-//        for (int i=0;i<options.size();i++){
-//            results[i] = ((javax.swing.text.JTextComponent)(options.get(i))).getText();
-//        } return results;
-//    }
-//    public void addComponent(Component component,int xpos, int ypos){
-//        gbc.gridx = xpos;
-//        gbc.gridy = ypos;
-//        gbc.insets = new Insets(2,2,2,2);
-//        gbl.setConstraints(component,gbc);
-//        this.add(component);
-//    }
-//    public void addComponent(Component component,int xpos,int ypos,int
-//            anchor){
-//        gbc.anchor = anchor;
-//        addComponent(component,xpos,ypos);
-//    }
-//}
